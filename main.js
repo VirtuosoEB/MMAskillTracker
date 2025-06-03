f6799a56e68b69941e7e873fc87aa9cb634faebc
VirtuosoEB <bienvenueelijah@gmail.com>
2025-06-03 14:05:27 -0400

    update

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('Hello World!');
+function moveCircle() {
+  console.log("Clicked");
+  let circle = document.getElementById("circle");
+  let arrow = document.getElementById("arrow");
+  circle.style.top = "20%"
+  arrow.style.top = "0%"
+  arrow.style.transform = "rotate(180deg)"
+}
