6bcd7050b2c80c889a8006fbeb20328dc0e01dd2
VirtuosoEB <bienvenueelijah@gmail.com>
2025-06-04 12:26:56 -0400

    update

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
 console.log('Hello World!');
 let state = 0
-function moveCircle() {
-  let circle = document.getElementById("circle");
-  let arrow = document.getElementById("arrow");
-  if (state === 0){
-    circle.style.top = "20%";
-    arrow.style.top = "0%";
-    arrow.style.transform = "rotate(180deg)";
-    state = 1
-    console.log("going back")
-  } else{
-      circle.style.top = "80%";
-    arrow.style.top = "60%";
-    arrow.style.transform = "rotate(0deg)";
-    state = 0
-    console.log("return")
-  }
-    console.log("Clicked" + state);
-}
+import {moveCircle} from "/modules/circleMovement.js"
+moveCircle()
