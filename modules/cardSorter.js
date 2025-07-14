fa2a9a1e68960832a7f2c744adb00ca4fb1b7478
VirtuosoEB <bienvenueelijah@gmail.com>
2025-07-14 11:07:33 -0400

    update

--- modules/cardSorter.js
+++ modules/cardSorter.js
@@ -1,1 +1,10 @@
-let cardList = JSON.parse(localStorage("cards"))
+let cards = JSON.parse(localStorage("cards"))
+function sortCards(){
+  let category1 = []
+  let category2 = []
+  let category3 = []
+  let category4 = []
+  let category5 = []
+  for(let i = 0; i<cards.length; i++){
+  }
+}
--- modules/editCards.js
+++ modules/editCards.js
@@ -1,9 +1,30 @@
+//getting card List
+let cards = JSON.parse(localStorage.getItem("cards"))
+let cardImage = 0
+
+
 export function editSelection(x) {
-  let cardHeading = x.getElementsByTagName("h1")[0].textContent
-  let cardImage = x.getElementsByTagName("img")[0].src
-  let cardNote = x.getElementsByTagName("p")[0].textContent
-  let cardId = x.id
+  let cardHeading = x.getElementsByTagName("h1")[0].textContent// sets heading
   
+  let cardNote = x.getElementsByTagName("p")[0].textContent
+  let cardId = x.id//sets note
+  
+  //=== gets category type===
+  let cardImage = 0
+  for (let i = 0; i < cards.length; i++){
+      let identity = cards[i].id
+      
+      if (String(identity) === String(cardId)){
+        console.log("found it!🫰")
+        cardImage = cards[i].type
+        break
+      } else {
+        console.log("nothing has been found yet")
+      }
+      
+    }
+  
+  //=== compiles info ===
   let card = {
     name: cardHeading, 
     type: cardImage, 
